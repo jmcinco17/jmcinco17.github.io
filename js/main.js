9153de4a423642bd5828a5705407a3adc3df8a77
John <johnmark.cinco@edu.sait.ca>
2024-11-17 15:20:18 -0700

    feat: add dynamic year and alert button

---
+++ js/main.js
@@ -0,0 +1,8 @@
+//Alert message
+  alert("Welcome to John Cinco's Portfolio! Please note that this website is still under development. Some features may be incomplete. Thank you for your patience!");
+
+// LiveUpdate Year
+const Year = document.getElementById("Year");
+const d = new Date();
+Year.innerText = d.getFullYear();
+
